fffa7fb54de7fe313ab62baa8b2d371cbed5ff14
kkoemets <koemets.kristjan@gmail.com>
2023-05-04 22:46:26 +0300

    Add option to config chat modes

--- bot/config.py
+++ bot/config.py
@@ -18,13 +18,16 @@ use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
+enabled_chat_models = config_yaml.get("enabled_chat_models", ["assistant"])
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
 
 # chat_modes
+
 with open(config_dir / "chat_modes.yml", 'r') as f:
-    chat_modes = yaml.safe_load(f)
+    all_chat_modes = yaml.safe_load(f)
+    chat_modes = {c: all_chat_modes[c] for c in enabled_chat_models}
 
 # models
 with open(config_dir / "models.yml", 'r') as f:
