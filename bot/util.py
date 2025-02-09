bea9918c19e4d69f1c33b021526df25087235238
kkoemets <koemets.kristjan@gmail.com>
2025-02-09 19:05:24 +0200

    Fix message crash due to invalid markdown

--- bot/util.py
+++ bot/util.py
@@ -1,3 +1,2 @@
 def split_text_into_chunks(text, chunk_size):
-    for i in range(0, len(text), chunk_size):
-        yield text[i:i + chunk_size]
+    return [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
